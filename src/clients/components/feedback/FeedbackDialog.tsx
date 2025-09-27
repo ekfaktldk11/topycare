776cd70c435b93961d3b1816826be58db0c060a8
ekfaktldk11 <ekfaktldk11@gmail.com>
2025-09-27 18:00:10 +0900

    close button

--- src/clients/components/feedback/FeedbackDialog.tsx
+++ src/clients/components/feedback/FeedbackDialog.tsx
@@ -1,4 +1,5 @@
-import { Dialog, DialogContent, Stack } from "@mui/material";
+import { Dialog, DialogContent, DialogTitle, IconButton, Stack } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 import FeedbackDialogHeader from "./FeedbackDialogHeader";
 import FeedbackList from "./FeedbackList";
 import FeedbackForm from "./FeedbackForm";
@@ -21,6 +22,12 @@ export default function FeedbackDialog({
 }: FeedbackDialogProps) {
     return (
         <Dialog open={open} onClose={onClose} fullWidth maxWidth="sm">
+            <DialogTitle sx={{ p: 2, display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                <span>Feedback</span>
+                <IconButton onClick={onClose} size="small" aria-label="close">
+                    <CloseIcon />
+                </IconButton>
+            </DialogTitle>
             <DialogContent sx={{ p: 0, display: "flex", flexDirection: "column", height: "70vh" }}>
                 <FeedbackDialogHeader dish={dish} />
                 <Stack sx={{ flex: 1, overflow: "auto" }}>
