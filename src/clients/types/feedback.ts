dd6be819316ede455f84f542cdea89678bdc32d4
ekfaktldk11 <ekfaktldk11@gmail.com>
2025-10-09 17:51:26 +0900

    modify type feedback - check annotation - remove dummy

--- src/clients/types/feedback.ts
+++ src/clients/types/feedback.ts
@@ -1,6 +1,13 @@
 // "유저들의 상품에 대한 평가" 를 의미하는 타입이름
+
+/**
+ * pk : itemId & userID & productType 로,
+ * 이렇게 하면, 한 유저가 한 상품에 대해 여러개의 평가를 남길 수 없게 되는데 이것은 의도다.
+ * 의도인 이유는 도배성 Feedback 아이템을 추가할 수 없게 하기위함임.
+ */
+
 export interface Feedback {
-    id: number; // 평가 ID
+    itemId: string; // 평가 ID
     userId: number; // 평가를 남긴 유저 ID
     productType: 'dish'; // | 'drink' | 'snack'; // 평가 대상 상품 유형
     productId: number; // 평가 대상 상품 ID
