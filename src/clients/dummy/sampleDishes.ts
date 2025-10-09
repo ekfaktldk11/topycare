b8018789ceb29093c7b3d763c737027b5a391577
ekfaktldk11 <ekfaktldk11@gmail.com>
2025-10-09 17:50:29 +0900

    modify dish type and dummmy data

--- src/clients/dummy/sampleDishes.ts
+++ src/clients/dummy/sampleDishes.ts
@@ -1,8 +1,8 @@
 import type { Dish } from "../types/dish";
 
 export const sampleDishes: Dish[] = [
-    { img: "https://picsum.photos/seed/1/400/400", name: "콜드브루 커피", brand: "무명브랜드", affect: 0.3 },
-    { img: "https://picsum.photos/seed/2/400/400", name: "제로 사이다", brand: "제로컴퍼니", affect: 3.5 },
-    { img: "https://picsum.photos/seed/3/400/400", name: "단백질 바", brand: "프로틴랩", affect: 5 },
-    { img: "https://picsum.photos/seed/4/400/400", name: "그릭요거트", brand: "헬씨팜", affect: 4 },
+    { id: '1', img: "https://picsum.photos/seed/1/400/400", name: "콜드브루 커피", brand: "무명브랜드", affect: 0.3 },
+    { id: '2', img: "https://picsum.photos/seed/2/400/400", name: "제로 사이다", brand: "제로컴퍼니", affect: 3.5 },
+    { id: '3', img: "https://picsum.photos/seed/3/400/400", name: "단백질 바", brand: "프로틴랩", affect: 5 },
+    { id: '4', img: "https://picsum.photos/seed/4/400/400", name: "그릭요거트", brand: "헬씨팜", affect: 4 },
 ];
