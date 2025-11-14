cb7c51d4c750fcd78ddeba0b6da860c0123238f8
ekfaktldk11 <ekfaktldk11@gmail.com>
2025-11-14 17:24:39 +0900

    feedback 타입을 스키마의 타입으로 설정 - dish 도 이렇게 수정해야할 것 같네

--- src/clients/types/feedback.ts
+++ src/clients/types/feedback.ts
@@ -1,11 +1,4 @@
+import type { Schema } from "../../../amplify/data/resource";
+
 // "유저들의 상품에 대한 평가" 를 의미하는 타입이름
-export interface Feedback {
-    id: string; // 고유 ID
-    itemId: string; // 평가 대상 상품 ID
-    //userId: string; // 평가를 남긴 유저 ID
-    itemType: 'dish'; // | 'drink' | 'snack'; // 평가 대상 상품 유형
-    rating: number; // 평점 (예: 1-5)
-    content: string; // 평가 내용
-    createdAt: Date; // 평가 작성일
-    updatedAt: Date; // 평가 수정일
-}
+export type Feedback = Schema["Feedback"]["type"];
